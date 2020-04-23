3fa72928d6a13f84a274f0a75ba217c131354982
dariak0108 <52563168+DariaK0108@users.noreply.github.com>
2020-04-23 20:22:56 +0200

    add files

---
+++ application.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# setup db
+db = SQLAlchemy()
+
+def create_app(**config_overrides):
+    app = Flask(__name__)
+    
+    # load config
+    app.config.from_pyfile('settings.py')
+
+    # apply overrides for tests
+    app.config.update(config_overrides)
+
+    # initialize db
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    from counter.views import counter_app
+    
+    app.register_blueprint(counter_app)
+
+    return app
